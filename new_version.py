77cc4b5fb2f3c0620fbd2332f731cabb5f4aa81c
dejanakop <dkop@griddynamics.com>
2025-11-06 21:32:09 +0100

    Update new_version.py
    Signed-off-by: dejanakop <dkop@griddynamics.com>

--- new_version.py
+++ new_version.py
@@ -1,7 +1,11 @@
 import subprocess
 
 def get_current_tag():
-  return subprocess.run(["git", "describe", "--tags", "--abbrev=0"])
+  try:
+    tag = subprocess.run(["git", "describe", "--tags", "--abbrev=0"], check=True, capture_output=True, text=True)
+  except subprocess.CalledProcessError as e:
+    tag = "0.0.0"
+  return tag
 
 if __name__ == "__main__":
 
