83142eca8d07905628759944863cf874426fd03e
dejanakop <dkop@griddynamics.com>
2025-11-15 22:58:30 +0100

    Delete new_version.py
    Signed-off-by: dejanakop <dkop@griddynamics.com>

--- new_version.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def get_current_tag():
-    return subprocess.run(["bash", "get_current_tag.sh"], capture_output=True, text=True)
-
-if __name__ == "__main__":
-
-  current_tag = get_current_tag()
-  print(current_tag)
